d7a10baf6b54569295c280033141e8023b0f2e83
psalmistcom <dadewale9@gmail.com>
2023-08-29 07:52:50 +0100

    added mail notification

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Notifications\WelcomeNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('welcome');
+        $user = User::first();
+
+        Notification::send($user, new WelcomeNotification);
+        dd('done');
+        // return view('welcome');
     }
 }
